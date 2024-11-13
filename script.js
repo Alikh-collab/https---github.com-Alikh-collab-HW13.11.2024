19c79340bca425be4ec3ba6be21f1b5da6cac144
Alikhan <chort445@gmail.com>
2024-11-13 20:21:03 +0300

    HW

---
+++ script.js
@@ -0,0 +1,22 @@
+let surak1;
+
+let surak2; 
+
+let surak;
+
+let ochko = 8;
+
+do{
+    surak = prompt("Menin atym kim?")
+    ochko--
+}while (surak != "alikhan")
+do{
+    surak = prompt("Qazaq handygy qashan kuryldy?")
+    ochko--
+}while (surak != "1465")
+    do{
+        surak = prompt("'lunch' ol bizde?:")
+        ochko--
+    }while (surak != "obed")
+
+alert("Sen " + ochko + " jinadyn")
